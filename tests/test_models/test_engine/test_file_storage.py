a9b8a734c9e36b180fc087dc3c6848f784e88f74
P4ver <s.pales20@gmail.com>
2024-01-13 14:36:48 -0800

    tsk

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,16 +3,20 @@
 Module for flstrge,
 """
 import unittest
+import models
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
-class TestFileStorage(unittes.TestCase):
+class TestFileStorage(unittest.TestCase):
     """
     Unittest for Filestorage cls,
     """
-    def test_inst(self):
-        self.assertEqual(type(FileStorage()), FileStorage)
+
+    @classmethod
+    def setUp(cls):
+        cls.file_storage0 = FileStorage()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
