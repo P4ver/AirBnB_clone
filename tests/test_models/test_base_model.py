0b9a4d33eeef9f15652215caf475ba4f72ca5559
P4ver <s.pales20@gmail.com>
2024-01-13 06:46:01 -0800

    tsk

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+Define Unittest,
 """
 import unittest
 from datetime import datetime
@@ -7,5 +8,12 @@ from models.base_model import BaseModel
 
 
 class TestBaseModel(unitest.TestCase):
+    """
+    Unittest tsting inst,
+    """
     def test_inst(self):
         self.assertIn(BaseModel(), models.storage.all().values())
+
+
+if __name__ == "__main__":
+    unittest.main()
